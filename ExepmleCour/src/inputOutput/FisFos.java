45e521ebb538bdce9eda421f6460b0b7660a8203
Client12@EST.LAN <Client12@EST.LAN>
2019-10-30 10:47:30 +0000

    new FisFos

--- ExepmleCour/src/inputOutput/FisFos.java
+++ ExepmleCour/src/inputOutput/FisFos.java
@@ -7,24 +7,29 @@ public class FisFos {
 	public static void main(String[] args) {
 		
 		FileInputStream fis = null;
+		FileOutputStream fos = null;
 		
-		
+		File g = new File("FileOut.txt");
 		File f = new File("fichier.txt");
 		
 		try {
 		 fis = new FileInputStream(f);
-		
+		 fos = new FileOutputStream(g);
+		  
 		byte buf[]= new byte[1];
 		int n ;
+		long t=System.currentTimeMillis();
 		while((n = fis.read(buf))>=0)
 		{
+			for(int i =0;i<n;i++)
+			fos.write(buf[i]);
 			
-			System.out.print((char)buf[0]);
 		}
-		 		
+		 		System.out.print("Time : " + t+ "ms");
 		}catch (IOException e) {
 			e.printStackTrace();
-		}finally {
+		}
+		finally {
 			try {
 				fis.close();
 			}
@@ -33,7 +38,10 @@ public class FisFos {
 				e.printStackTrace();
 			}
 		}
-			
+		
+		
+		
+		
 		           
 		
 	}
