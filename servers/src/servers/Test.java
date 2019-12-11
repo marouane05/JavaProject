e685c17ce887e7e659f3dd723d30757e7df38241
isacc10 <ishak.zail.iz@gmail.com>
2019-12-11 11:00:05 +0000

    servers

---
+++ servers/src/servers/Test.java
@@ -0,0 +1,16 @@
+package servers;
+
+public class Test {
+	public static void main(String args[]) {
+		ThreadPooledServer server = new ThreadPooledServer(9000);
+		new Thread(server).start();
+
+		try {
+		    Thread.sleep(20 * 1000);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
+		System.out.println("Stopping Server");
+		server.stop();
+	}
+}
